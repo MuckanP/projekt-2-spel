48bdcf89ce8a461dbc1379e2302eb33be76595c0
EdvinTetek23 <Edvin.Hasselgren@elev.ga.dbgy.se>
2026-02-06 10:50:25 +0100

    Settings done

--- settings.py
+++ settings.py
@@ -1,2 +1,13 @@
 Width, Height = 800, 400
 FPS = 60
+gravity = 0.7
+jump_force = --16
+scroll_speed = 5
+
+tile_size = 40
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (220, 50, 50)
+GRAY = (120, 120, 120)
+GREEN = (50, 220, 50)
