90abefc25f3e84b225aad57414ce56a47d80491e
MuckanP <maximilian.plasberg@elev.ga.dbgy.se>
2026-02-06 10:48:40 +0100

    init player

--- player.py
+++ player.py
@@ -3,3 +3,9 @@ from settings import ()
 
 class Player:
     def __init__(self, x, y):
+        self.rect = pygame.Rect(x, y, 40,40)
+        self.vel_y = 0
+        self.on_ground = False
+    
+    def draw(self, screen):
+        pygame.draw.rect(screen, GREEN, self.rect)
