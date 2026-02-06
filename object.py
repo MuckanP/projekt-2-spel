e229d3e2ddfb1e461968ea202f5c62bf20300821
MuckanP <maximilian.plasberg@elev.ga.dbgy.se>
2026-02-06 11:09:00 +0100

    object done for now

--- object.py
+++ object.py
@@ -11,3 +11,12 @@ class Block:
     def draw(self, screen):
         pygame.draw.rect(screen, GRAY, self.rect)
 
+class Spike: 
+    def __init__(self , x, y, w, h):
+        self.rect = pygame.Rect(x, y, w, h)
+    
+    def update(self, scroll_speed):
+        self.rect.x -= scroll_speed
+    
+    def draw(self, screen):
+        pygame.draw.rect(screen, RED, self.rect)
