e8561148e390a262549100e1e258de1c32fd3864
EdvinTetek23 <Edvin.Hasselgren@elev.ga.dbgy.se>
2026-02-13 10:41:44 +0100

    player fungerar

--- player.py
+++ player.py
@@ -6,6 +6,25 @@ class Player:
         self.rect = pygame.Rect(x, y, 40,40)
         self.vel_y = 0
         self.on_ground = False
+        
+    def update(self, blocks):
+        self.rect = GRAVITY
+        self.vel_y = self.vel_y
+        self.on_ground = False
+        
+        for block in blocks:
+            if self.rect.colliderect(block.rect):
+                if self.vel_y > 0:
+                    self.rect.bottom = block.rect.top
+                    self.vel_y = 0
+                    self.on_ground = True
+                    
+    def jump(self):
+        if self.on_ground:
+            self.vel_y = JUMP_FORCE
+
     
     def draw(self, screen):
         pygame.draw.rect(screen, GREEN, self.rect)
+        
+        
