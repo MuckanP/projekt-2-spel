e5d7a2185734948bcc899b40ed00688119727520
MuckanP <maximilian.plasberg@elev.ga.dbgy.se>
2026-03-06 12:04:53 +0100

    fixat settings

--- settings.py
+++ settings.py
@@ -1,13 +1,15 @@
 Width, Height = 800, 400
 FPS = 60
-gravity = 0.7
-jump_force = --16
-scroll_speed = 5
 
-tile_size = 40
+GRAVITY = 0.7
+JUMP_FORCE = --16
+SCROLL_SPEED = 5
+TILE_SIZE = 40
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (220, 50, 50)
 GRAY = (120, 120, 120)
 GREEN = (50, 220, 50)
+
+HEIGHT = 100 #denna kanske funkar? löser bug for now
