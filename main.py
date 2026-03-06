7191dcb43e6ef2fae5d98328ab795b8c3c5009e4
MuckanP <maximilian.plasberg@elev.ga.dbgy.se>
2026-03-06 11:13:08 +0100

    main fixad typ ish

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import pygame
 from settings import *
 from player import Player
@@ -9,7 +10,7 @@ screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 
-game_state = "menu"
+game_state = "game" #ändra till menu när menu funkar
 
 player = Player(100, HEIGHT - 80)
 level = Level("levels/level1.txt")
@@ -25,23 +26,29 @@ while running:
             pygame.quit()
             sys.exit()
             
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                player.jump()
-    player.update(level.blocks)
-    level.update(SCROLL_SPEED)
+        if game_state == "menu":
+            if event.type == pygame.KEYDOWN:
+                game_state = "game"
+            if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                sys.exit()
+                
+        elif game_state == "game":
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    player.jump()
     
     # if game_state == "menu": # meny finns inte än så länge
     #     menu.draw(screen) # kommer bygga en meny på detta
         
-    if game_state == "game": # byt till elif när övre raden funkar
+    if game_state == "game": 
         player.update(level.blocks)
         level.update(SCROLL_SPEED)
         
         for spike in level.spikes: # för spike collisions 
             if player.rect.colliderect(spike.rect):
                 attempts + 1
-                print("Attempt " attempts)
+                print("Attempt ", attempts)
         
         screen.fill(WHITE)
         level.draw(screen)
